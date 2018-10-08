5dff9ddc4bab51ae7d778fb857516c5df478d348
Vlada Dusek <v.dusek96@gmail.com>
2018-10-08 20:41:10 +0200

    Update pcap_parser, it parses dns header

--- src/pcap_parser.cpp
+++ src/pcap_parser.cpp
@@ -8,123 +8,137 @@
 
 #include <string>
 #include <iostream>
+
+#include <sys/socket.h>
 #include <pcap/pcap.h>
 #include <net/ethernet.h>
+#include <netinet/ether.h>
 #include <netinet/in.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+
 #include "pcap_parser.h"
 
 using namespace std;
 
-int ip_cnt = 0,
-        arp_cnt = 0,
-        revarp_cnt = 0,
-        other_cnt = 0,
-        tcp_cnt = 0,
-        udp_cnt = 0;
+u_int n = 0,
+      ip_cnt = 0,
+      arp_cnt = 0,
+      ipv6_cnt = 0,
+      other_cnt = 0,
+      ip_tcp_cnt = 0,
+      ip_udp_cnt = 0,
+      ip_other_cnt = 0;
 
 struct dns_header
 {
-    unsigned short id; // identification number
-
-    unsigned char rd :1; // recursion desired
-    unsigned char tc :1; // truncated message
-    unsigned char aa :1; // authoritive answer
-    unsigned char opcode :4; // purpose of message
-    unsigned char qr :1; // query/response flag
-
-    unsigned char rcode :4; // response code
-    unsigned char cd :1; // checking disabled
-    unsigned char ad :1; // authenticated data
-    unsigned char z :1; // its z! reserved
-    unsigned char ra :1; // recursion available
-
-    unsigned short q_count; // number of question entries
-    unsigned short ans_count; // number of answer entries
-    unsigned short auth_count; // number of authority entries
-    unsigned short add_count; // number of resource entries
+    u_int16_t id;
+    u_int16_t flags;
+    u_int16_t qd_count;
+    u_int16_t an_count;
+    u_int16_t ns_count;
+    u_int16_t ar_count;
 };
 
-void my_packet_handler(u_char *args, const struct pcap_pkthdr *packet_header, const u_char *packet_body)
+void my_packet_handler(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 {
-    struct ether_header *ether_hdr = (struct ether_header *) packet_body;
-    const u_char *ip_hdr = nullptr;
-    struct dns_header *dns_hdr = nullptr;
-    u_char protocol;
+    struct ether_header *ether_hdr = nullptr;
+    struct ip *ip_hdr = nullptr;
+    struct udphdr *udp_hdr = nullptr;
+    void *dns_hdr = nullptr;
+    dns_header *dns;
 
-    int ethernet_header_length = 14; /* Doesn't change */
-    int ip_header_length;
-    int udp_header_length = 8; /* Doesn't change */
-    int dns_header_length = 12; /* Doesn't change */
+    u_int ethernet_header_length = 14; /* Doesn't change */
+    u_int ip_header_length;
+    u_int udp_header_length = 8; /* Doesn't change */
+    u_int dns_header_length = 12; /* Doesn't change */
 
-    if (ntohs(ether_hdr->ether_type) == ETHERTYPE_IP) {
-        ip_cnt++;
+    n++;
+    ether_hdr = (struct ether_header *) packet;
+
+    switch (ntohs(ether_hdr->ether_type)) {
+
+        case ETHERTYPE_IP:
+            ip_cnt++;
+
+            /* Find start of IP header */
+            ip_hdr = (struct ip *) (packet + ethernet_header_length);
+            ip_header_length = ip_hdr->ip_hl * 4;
+
+            switch (ip_hdr->ip_p) {
+
+                case IPPROTO_UDP:
+                    ip_udp_cnt++;
+
+                    printf("Packet no. %d:\n",n);
+                    printf("    &packet: %u\n", packet);
+                    printf("    Length %d, received at %s", header->len, ctime((const time_t*) & header->ts.tv_sec));
+                    printf("    Source MAC: %s\n", ether_ntoa((const struct ether_addr *) & ether_hdr->ether_shost));
+                    printf("    Destination MAC: %s\n", ether_ntoa((const struct ether_addr *) & ether_hdr->ether_dhost));
+                    printf("    Ethernet type is  0x%x, i.e. IP packet \n", ntohs(ether_hdr->ether_type));
+                    printf("    IP: id 0x%x, hlen %d bytes, version %d, total length %d bytes, TTL %d\n",
+                           ntohs(ip_hdr->ip_id), ip_header_length, ip_hdr->ip_v, ntohs(ip_hdr->ip_len), ip_hdr->ip_ttl);
+                    printf("    IP src = %s, ", inet_ntoa(ip_hdr->ip_src));
+                    printf("IP dst = %s", inet_ntoa(ip_hdr->ip_dst));
+
+                    printf(", protocol UDP (%d)\n", ip_hdr->ip_p);
+
+                    dns_hdr = (void *) (packet + ethernet_header_length + ip_header_length + udp_header_length);
+
+                    printf("Ethernet header length: %d\n", ethernet_header_length);
+                    printf("IP header length:       %d\n", ip_header_length);
+                    printf("UDP header length:      %d\n", udp_header_length);
+
+                    dns = (dns_header *) dns_hdr;
+
+                    printf("DNS header:\n");
+                    printf("    &dns_hdr: %d\n", dns_hdr);
+                    printf("    id = 0x%04x\n", ntohs(dns->id));
+                    printf("    flags = 0x%04x\n", ntohs(dns->flags));
+                    printf("    qd_count = %d\n", ntohs(dns->qd_count));
+                    printf("    an_count = %d\n", ntohs(dns->an_count));
+                    printf("    ns_count = %d\n", ntohs(dns->ns_count));
+                    printf("    ar_count = %d\n", ntohs(dns->ar_count));
+
+                    cout << endl;
+
+                    if (ntohs(dns->id) == 0xab47 && (dns->flags & 0b1000000000000000)) {
+                        exit(0);
+                    }
+
+                    break;
+
+                case IPPROTO_TCP:
+                    ip_tcp_cnt++;
+                    break;
+
+                default:
+                    ip_other_cnt++;
+                    break;
+            }
+            break;
+
+        case ETHERTYPE_ARP:
+            arp_cnt++;
+            break;
+
+        case ETHERTYPE_IPV6:
+            ipv6_cnt++;
+            break;
+
+        default:
+            other_cnt++;
+            break;
     }
-    else  if (ntohs(ether_hdr->ether_type) == ETHERTYPE_ARP) {
-        arp_cnt++;
-        return;
-    }
-    else  if (ntohs(ether_hdr->ether_type) == ETHERTYPE_REVARP) {
-        revarp_cnt++;
-        return;
-    }
-    else {
-        other_cnt++;
-        return;
-    }
-
-    /* Find start of IP header */
-    ip_hdr = packet_body + ethernet_header_length;
-
-    /* The second-half of the first byte in ip_hdr contains
-     * the IP header length (IHL). */
-    ip_header_length = ((*ip_hdr) & 0x0F);
-
-    /* The IHL is number of 32-bit segments. Multiply by four
-     * to get a byte count for pointer arithmetic */
-    ip_header_length = ip_header_length * 4;
-
-    /* Now that we know where the IP header is, we can inspect
-     * the IP header for a protocol number to make sure it is
-     * TCP before going any further. Protocol is always the 10th
-     * byte of the IP header */
-    protocol = *(ip_hdr + 9);
-
-    if (protocol == IPPROTO_TCP) {
-        tcp_cnt++;
-        return;
-    }
-    else if (protocol == IPPROTO_UDP) {
-        udp_cnt++;
-    }
-    else {
-        return;
-    }
-
-    printf("&packet_header = %d , %p\n", packet_header, packet_header);
-    printf("&packet_body = %d , %p\n", packet_body, packet_body);
-
-    printf("IP -> UDP packet found!\n");
-    printf("Packet capture length: %d\n", packet_header->caplen);
-    printf("Packet total length %d\n", packet_header->len);
-    printf("Ethernet header length: %d\n", ethernet_header_length);
-    printf("IP header length: %d\n", ip_header_length);
-    printf("UDP header length: %d\n", udp_header_length);
-
-    dns_hdr = (dns_header *) packet_body + ethernet_header_length + ip_header_length + udp_header_length;
-    printf("&dns_hdr = %d , %p\n", dns_hdr, dns_hdr);
-//    printf("&packet_body - &dns_hdr = %d\n", static_cast<void *> (packet_body) - static_cast<void *> (dns_hdr));
 
 
-    cout << "DNS header:" << endl;
-    cout << "    id = " << dns_hdr->id << endl;
-    cout << "    qr = " << dns_hdr->qr << endl;
-    cout << "    rcode = " << dns_hdr->rcode << endl;
-    cout << "    qdcount = " << dns_hdr->q_count << endl;
-    cout << "    ans_count = " << dns_hdr->ans_count << endl;
-    cout << "    auth_count = " << dns_hdr->auth_count << endl;
-    cout << "    add_count = " << dns_hdr->add_count << endl;
 
-    cout << endl;
+
+
+
+
+
+
 }
 
 PcapParser::PcapParser(std::string filename):
@@ -157,13 +171,14 @@ void PcapParser::parse()
     pcap_close(handle);
 
     cout << endl;
-    cout << "Summary:" << endl;
-    cout << "IP:     " << ip_cnt << endl;
-    cout << "    - TCP = " << tcp_cnt << endl;
-    cout << "    - UDP = " << udp_cnt << endl;
-    cout << "ARP:    " << arp_cnt << endl;
-    cout << "REVARP: " << revarp_cnt << endl;
-    cout << "OTHER:  " << other_cnt << endl;
+    cout << "Summary: " << endl;
+    cout << "IP = " << ip_cnt << endl;
+    cout << "    TCP = " << ip_tcp_cnt << endl;
+    cout << "    UDP = " << ip_udp_cnt << endl;
+    cout << "    other = " << ip_other_cnt << endl;
+    cout << "ARP = " << arp_cnt << endl;
+    cout << "IPv6 = " << ipv6_cnt << endl;
+    cout << "other = " << other_cnt << endl;
 
     // ToDo
 }
