83ae9a6e5925555bb281b9b85c918620c79c649c
Vlada Dusek <v.dusek96@gmail.com>
2018-10-10 08:48:22 +0200

    Parses pcap file successfully

--- src/pcap_parser.cpp
+++ src/pcap_parser.cpp
@@ -8,7 +8,12 @@
 
 #include <string>
 #include <iostream>
+#include <bitset>
+#include <vector>
 
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <pcap/pcap.h>
 #include <net/ethernet.h>
@@ -16,11 +21,13 @@
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <arpa/inet.h>
+#include <linux/udp.h>
 
 #include "pcap_parser.h"
 
 using namespace std;
 
+// debug
 u_int n = 0,
       ip_cnt = 0,
       arp_cnt = 0,
@@ -28,117 +35,289 @@ u_int n = 0,
       other_cnt = 0,
       ip_tcp_cnt = 0,
       ip_udp_cnt = 0,
-      ip_other_cnt = 0;
+      ip_other_cnt = 0,
+      dns_cnt = 0;
 
-struct dns_header
+/*
+ * struct in6_addr {
+ *     unsigned char   s6_addr[16];   // IPv6 address
+ * };
+ *
+ * INET6_ADDRSTRLEN = 46;
+ *
+ * // Convert IPv4 and IPv6 addresses from binary to text form
+ * const char *inet_ntop(int af, const void *src, char *dst, socklen_t size);
+ */
+string read_ipv6(u_char *dns_reader)
 {
-    u_int16_t id;
-    u_int16_t flags;
-    u_int16_t qd_count;
-    u_int16_t an_count;
-    u_int16_t ns_count;
-    u_int16_t ar_count;
-};
+    in6_addr address;
+    memcpy(&address, dns_reader, sizeof(address));
+    char ipv6[INET6_ADDRSTRLEN] = {0};
+    inet_ntop(AF_INET6, &address, ipv6, INET6_ADDRSTRLEN);
+    return static_cast<string> (ipv6);
+}
 
-void my_packet_handler(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
+/*
+ * struct in_addr {
+ *     unsigned long s_addr;  // load with inet_aton()
+ * };
+ * INET6_ADDRSTRLEN = 16;
+ */
+string read_ipv4(u_char *dns_reader)
 {
-    struct ether_header *ether_hdr = nullptr;
-    struct ip *ip_hdr = nullptr;
-    struct udphdr *udp_hdr = nullptr;
-    void *dns_hdr = nullptr;
-    dns_header *dns;
+    in_addr address;
+    memcpy(&address, dns_reader, sizeof(address));
+    char ipv4[INET_ADDRSTRLEN] = {0};
+    inet_ntop(AF_INET, &address, ipv4, INET_ADDRSTRLEN);
+    return static_cast<string> (ipv4);
+}
 
-    u_int ethernet_header_length = 14; /* Doesn't change */
-    u_int ip_header_length;
-    u_int udp_header_length = 8; /* Doesn't change */
-    u_int dns_header_length = 12; /* Doesn't change */
+string read_name(u_char *dns_hdr, u_char *dns, u_int32_t *shift)
+{
+    string name;
+    u_int32_t offset;
+    bool ptr = false;
 
-    n++;
-    ether_hdr = (struct ether_header *) packet;
+    while (*dns != '\0') {
+        /* "The significance of the compression label is as follows: the first 2 bits are set to 1,
+         * the 14 remaining bits describe the offset, i.e. the position of the compression target
+         * from the beginning of the DNS message." */
+        if (*dns >= 0b11000000) {
+            offset = (u_int32_t) ((*dns) * 0x100 + *(dns + 1) - 0xC000); // calculation of offset
+            dns = dns_hdr + offset;
+            ptr = true;
+        }
 
-    switch (ntohs(ether_hdr->ether_type)) {
+        for (int cnt = *dns; cnt > 0; cnt--) {
+            dns++;
+            name += *dns;
+        }
+        name += '.';
+        dns++;
+    }
+
+    name.pop_back();
+    name += '\0';
+
+    /* If't was pointer, shift just 2 bytes */
+    if (ptr) {
+        *shift = 2;
+    }
+    /* If't was a whole dns name, shift length of the string + 1 */
+    else {
+        *shift = (u_int32_t) name.length() + 1;
+    }
+
+    return name;
+}
+
+void packet_handler(u_char *args, const struct pcap_pkthdr *packet_hdr, const u_char *packet)
+{
+    (void) args; (void) packet_hdr; // Stop yelling at me!
+
+    struct ether_header *eth     = nullptr;
+    struct ip           *ip_     = nullptr;
+    struct udphdr       *udp     = nullptr;
+    DnsHeader          *dns_hdr = nullptr;
+    DnsAnswer          *dns_ans = nullptr;
+    u_char              *dns     = nullptr;
+
+    const u_int eth_len     = 14,
+                udp_len     = 8,
+                dns_hdr_len = 12;
+
+    u_int ip_len,
+          shift;
+
+    string name,
+           data,
+           type,
+           result;
+
+
+    n++; // debug
+    eth = (ether_header *) packet;
+
+    /* Filter just IP frames */
+    switch (ntohs(eth->ether_type)) {
 
         case ETHERTYPE_IP:
             ip_cnt++;
-
-            /* Find start of IP header */
-            ip_hdr = (struct ip *) (packet + ethernet_header_length);
-            ip_header_length = ip_hdr->ip_hl * 4;
-
-            switch (ip_hdr->ip_p) {
-
-                case IPPROTO_UDP:
-                    ip_udp_cnt++;
-
-                    printf("Packet no. %d:\n",n);
-                    printf("    &packet: %u\n", packet);
-                    printf("    Length %d, received at %s", header->len, ctime((const time_t*) & header->ts.tv_sec));
-                    printf("    Source MAC: %s\n", ether_ntoa((const struct ether_addr *) & ether_hdr->ether_shost));
-                    printf("    Destination MAC: %s\n", ether_ntoa((const struct ether_addr *) & ether_hdr->ether_dhost));
-                    printf("    Ethernet type is  0x%x, i.e. IP packet \n", ntohs(ether_hdr->ether_type));
-                    printf("    IP: id 0x%x, hlen %d bytes, version %d, total length %d bytes, TTL %d\n",
-                           ntohs(ip_hdr->ip_id), ip_header_length, ip_hdr->ip_v, ntohs(ip_hdr->ip_len), ip_hdr->ip_ttl);
-                    printf("    IP src = %s, ", inet_ntoa(ip_hdr->ip_src));
-                    printf("IP dst = %s", inet_ntoa(ip_hdr->ip_dst));
-
-                    printf(", protocol UDP (%d)\n", ip_hdr->ip_p);
-
-                    dns_hdr = (void *) (packet + ethernet_header_length + ip_header_length + udp_header_length);
-
-                    printf("Ethernet header length: %d\n", ethernet_header_length);
-                    printf("IP header length:       %d\n", ip_header_length);
-                    printf("UDP header length:      %d\n", udp_header_length);
-
-                    dns = (dns_header *) dns_hdr;
-
-                    printf("DNS header:\n");
-                    printf("    &dns_hdr: %d\n", dns_hdr);
-                    printf("    id = 0x%04x\n", ntohs(dns->id));
-                    printf("    flags = 0x%04x\n", ntohs(dns->flags));
-                    printf("    qd_count = %d\n", ntohs(dns->qd_count));
-                    printf("    an_count = %d\n", ntohs(dns->an_count));
-                    printf("    ns_count = %d\n", ntohs(dns->ns_count));
-                    printf("    ar_count = %d\n", ntohs(dns->ar_count));
-
-                    cout << endl;
-
-                    if (ntohs(dns->id) == 0xab47 && (dns->flags & 0b1000000000000000)) {
-                        exit(0);
-                    }
-
-                    break;
-
-                case IPPROTO_TCP:
-                    ip_tcp_cnt++;
-                    break;
-
-                default:
-                    ip_other_cnt++;
-                    break;
-            }
+            ip_ = (ip *) (packet + eth_len);
+            ip_len = ip_->ip_hl * 4;
             break;
 
         case ETHERTYPE_ARP:
             arp_cnt++;
-            break;
+            return;
 
         case ETHERTYPE_IPV6:
             ipv6_cnt++;
-            break;
+            return;
 
         default:
             other_cnt++;
-            break;
+            return;
     }
 
+    /* Filter just UDP communication */
+    switch (ip_->ip_p) {
 
+        case IPPROTO_UDP:
+            ip_udp_cnt++;
+            udp = (udphdr *) ((char *) ip_ + ip_len);
+            break;
 
+        case IPPROTO_TCP:
+            ip_tcp_cnt++;
+            return;
 
+        default:
+            ip_other_cnt++;
+            return;
+    }
 
+    /* Filter just communication with source port 53 */
+    if (ntohs(udp->source) != 53) {
+        cerr << "Not source port 53" << endl;
+        return;
+    }
 
+    dns_hdr = (DnsHeader *) ((char *) udp + udp_len);
 
+    /* Filter just responses */
+    if (!(ntohs(dns_hdr->flags) & 0b1000000000000000)) {
+        cerr << "Not a response" << endl;
+        return;
+    }
 
+    /* Filter just no error responses */
+    if (ntohs(dns_hdr->flags) & 0b0000000000001111)  {
+        cerr << "There's an error in the response" << endl;
+        return;
+    }
 
+    /* Filter just responses with 1 question (other makes no sense)
+     * source: https://stackoverflow.com/questions/7565300/identifying-dns-packets */
+    if (ntohs(dns_hdr->qd_count) != 1) {
+        cerr << "There's no 1 question" << endl;
+        return;
+    }
+
+    /* Filter just responses with answers */
+    if (ntohs(dns_hdr->an_count) == 0) {
+        cerr << "There's no answer" << endl;
+        return;
+    }
+
+    dns_cnt++; // DNS traffic
+
+    fprintf(stderr, "------------------------------------------------------------------------------\n\n");
+    fprintf(stderr, "Packet no. %d\n", n);
+    fprintf(stderr, "    Length: %d\n", packet_hdr->len);
+    fprintf(stderr, "    Source MAC: %s\n", ether_ntoa((const struct ether_addr *) & eth->ether_shost));
+    fprintf(stderr, "    Destination MAC: %s\n", ether_ntoa((const struct ether_addr *) & eth->ether_dhost));
+    fprintf(stderr, "    Ethernet type: 0x%x (IP packet)\n", ntohs(eth->ether_type));
+    fprintf(stderr, "    IP: hlen %d bytes, version %d, total length %d bytes, TTL %d\n", ip_len,
+            ip_->ip_v, ntohs(ip_->ip_len), ip_->ip_ttl);
+    fprintf(stderr, "    IP src = %s, ", inet_ntoa(ip_->ip_src));
+    fprintf(stderr, "IP dst = %s", inet_ntoa(ip_->ip_dst));
+    fprintf(stderr, ", protocol UDP (%d)\n\n", ip_->ip_p);
+
+    fprintf(stderr, "DNS header\n");
+    fprintf(stderr, "    id = 0x%04x\n", ntohs(dns_hdr->id));
+    fprintf(stderr, "    flags = 0x%04x\n", ntohs(dns_hdr->flags));
+    fprintf(stderr, "    qd_count = %d\n", ntohs(dns_hdr->qd_count));
+    fprintf(stderr, "    an_count = %d\n", ntohs(dns_hdr->an_count));
+    fprintf(stderr, "    ns_count = %d\n", ntohs(dns_hdr->ns_count));
+    fprintf(stderr, "    ar_count = %d\n\n", ntohs(dns_hdr->ar_count));
+
+    dns = (u_char *) dns_hdr + dns_hdr_len;
+
+    /* Skip query (name + 2 + 2) */
+    while (*(++dns) != '\0')
+        ;
+    dns += (1 + sizeof(DnsQuery));
+
+    /* For every answer */
+    for (int i = 0; i < ntohs(dns_hdr->an_count); i++) {
+
+        fprintf(stderr, "DNS answer (%d)\n", i + 1);
+
+        name = read_name((u_char *) dns_hdr, dns, &shift);
+
+        fprintf(stderr, "    domain_name = %s\n", name.c_str());
+
+        dns_ans = (DnsAnswer *) (dns + shift);
+
+        fprintf(stderr, "    type = %d\n", ntohs(dns_ans->type));
+        fprintf(stderr, "    class = %d\n", ntohs(dns_ans->class_));
+        fprintf(stderr, "    ttl = %d\n", ntohl(dns_ans->ttl));
+        fprintf(stderr, "    data_len = %d\n", ntohs(dns_ans->data_len));
+
+        dns = dns + sizeof(DnsAnswer);
+
+        switch (ntohs(dns_ans->type)) {
+
+            case DNS_A:
+                data = read_ipv4(dns);
+                type = "A";
+                break;
+
+            case DNS_AAAA:
+                data = read_ipv6(dns);
+                type = "AAAA";
+                break;
+
+            case DNS_CNAME:
+                // Shift is not gonna be used
+                data = read_name((u_char *) dns_hdr, dns, &shift);
+                type = "CNAME";
+                break;
+
+            case DNS_DS:
+                break;
+
+            case DNS_MX:
+                break;
+
+            case DNS_NS:
+                break;
+
+            case DNS_NSEC:
+                break;
+
+            case DNS_PTR:
+                break;
+
+            case DNS_RRSIG:
+                break;
+
+            case DNS_SOA:
+                break;
+
+            case DNS_SPF:
+                break;
+
+            case DNS_TXT:
+                break;
+
+            default:
+                break;
+        }
+        dns += ntohs(dns_ans->data_len);
+
+        fprintf(stderr, "    data = %s\n\n", data.c_str());
+        fprintf(stderr, "%s %s %s\n\n", name.c_str(), type.c_str(), data.c_str());
+
+        result += name.append(" ") + type.append(" ") + data.append("\n");
+    }
+
+    cout << result;
+
+//    if (ntohs(dns_hdr->id) == 0xa2bc) {
+//        exit(0);
+//    }
 }
 
 PcapParser::PcapParser(std::string filename):
@@ -152,7 +331,6 @@ PcapParser::~PcapParser()
 
 void PcapParser::parse()
 {
-    cout << "parsuju " << m_filename << endl;
     char error_buffer[PCAP_ERRBUF_SIZE];
 
     pcap_t *handle = nullptr;
@@ -163,22 +341,23 @@ void PcapParser::parse()
     packet = pcap_next(handle, &packet_header);
 
     if (packet == NULL) {
-        printf("No packet found.\n");
+        cerr << "No packet found." << endl;
         return;
     }
 
-    pcap_loop(handle, 0, my_packet_handler, NULL);
+    pcap_loop(handle, 0, packet_handler, nullptr);
     pcap_close(handle);
 
     cout << endl;
     cout << "Summary: " << endl;
-    cout << "IP = " << ip_cnt << endl;
-    cout << "    TCP = " << ip_tcp_cnt << endl;
-    cout << "    UDP = " << ip_udp_cnt << endl;
-    cout << "    other = " << ip_other_cnt << endl;
     cout << "ARP = " << arp_cnt << endl;
     cout << "IPv6 = " << ipv6_cnt << endl;
     cout << "other = " << other_cnt << endl;
+    cout << "IP = " << ip_cnt << endl;
+    cout << "    other = " << ip_other_cnt << endl;
+    cout << "    TCP = " << ip_tcp_cnt << endl;
+    cout << "    UDP = " << ip_udp_cnt << endl;
+    cout << "        DNS = " << dns_cnt << endl;
 
     // ToDo
 }
--- src/pcap_parser.h
+++ src/pcap_parser.h
@@ -11,6 +11,50 @@
 #include <string>
 #include <pcap/pcap.h>
 
+enum TypeDnsRecord {
+    DNS_A = 1,
+    DNS_AAAA = 28,
+    DNS_CNAME = 5,
+    DNS_DS = 43,
+    DNS_MX = 15,
+    DNS_NS = 2,
+    DNS_NSEC = 47,
+    DNS_PTR = 12,
+    DNS_RRSIG = 46,
+    DNS_SOA = 6,
+    DNS_SPF = 99,
+    DNS_TXT = 16
+};
+
+struct DnsHeader {
+    u_int16_t id;
+    u_int16_t flags;
+    u_int16_t qd_count;
+    u_int16_t an_count;
+    u_int16_t ns_count;
+    u_int16_t ar_count;
+};
+
+struct DnsQuery {
+    // + name of the node whose resource records are being requested
+    u_int16_t type;
+    u_int16_t class_;
+};
+
+struct DnsAnswer {
+    // + encoded name of the node to which this resource record applies
+    u_int16_t type;
+    u_int16_t class_;
+    u_int32_t ttl;
+    u_int16_t data_len;
+    // + specific record data
+};
+
+void packet_handler(u_char *args, const struct pcap_pkthdr *packet_hdr, const u_char *packet);
+std::string read_name(u_char *dns_hdr, u_char *dns, u_int32_t *shift);
+std::string read_ipv4(u_char *dns_reader);
+std::string read_ipv6(u_char *dns_reader);
+
 class PcapParser {
 private:
     std::string m_filename;
